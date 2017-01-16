f66117e435b74290528a6509104c73cf4b48af29
WaldenLan <446731905@qq.com>
2017-01-16 14:56:37 +0800

    update the data export and basic optimization for MongoDB

--- MongoDB/MongoDB-Tensorflow/iris_bp.py
+++ MongoDB/MongoDB-Tensorflow/iris_bp.py
@@ -2,14 +2,17 @@ import tensorflow as tf
 import numpy as np
 from pymongo import MongoClient
 
-
-def mongodb_to_tensorflow(database, col, field):
-    # REQUIRE: The target datasets must be imported into the database
+def initialize_db_col(database, col):
     client      = MongoClient()
 
     # Access the target database and corresponding collection
     db          = client[database]
     collection  = db[col]
+
+    return collection
+
+def mongodb_to_tensorflow(collection, field):
+    # REQUIRE: The target datasets must be imported into the database
 
     # Fetch the key list in the database
     attr        = ["sepal_length", "sepal_width", "petal_length", "petal_width", "label"]
@@ -20,6 +23,10 @@ def mongodb_to_tensorflow(database, col, field):
 
     num_arr     = get_numpy_array(collection, attr, field)
     return num_arr
+
+def tensorflow_to_mongodb(collection, entry):
+    # Insert one entry into the collection
+    collection.insert_one(entry)
 
 def get_numpy_array(collection, attr, field):
     data = []
@@ -44,14 +51,16 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 
 # train data
 
-field    = (0,1,2,3)
-label    = (4,)
-database = "train"
-col      = "col"
+field       = (0,1,2,3)
+label       = (4,)
+database    = "train"
+col         = "col"
+# Initialize the database and the collection
+collection  = initialize_db_col(database, col)
 # read the first 4 columns
-x_data   = mongodb_to_tensorflow(database, col, field)
+x_data      = mongodb_to_tensorflow(collection, field)
 # read the fifth column
-target   = mongodb_to_tensorflow(database, col, label)
+target      = mongodb_to_tensorflow(collection, label)
 
 y_data = np.zeros((len(target),3))
 
@@ -72,11 +81,11 @@ ys = tf.placeholder(tf.float32, [None, 3])
 
 # define the layers
 # add hidden layer inpuy : xs 12unit   
-l1 = add_layer(xs, 4, 12, activation_function=tf.nn.relu)
+l1          = add_layer(xs, 4, 12, activation_function=tf.nn.relu)
 # add output layer input: l1  output  3 classes
 prediction1 = add_layer(l1, 12, 3, activation_function=None)
 # add softmax layer
-prediction = tf.nn.softmax(prediction1)
+prediction  = tf.nn.softmax(prediction1)
 
 
 # define loss
@@ -99,39 +108,49 @@ sess = tf.Session()
 sess.run(init)
 
 # sess.run optimizer
+# Initialize the output database and the collection
+database    = "train_result"
+col         = "bp_iris"
+collection  = initialize_db_col(database, col)
 for i in range(5000):
     # training train_step & loss , placeholder ,feed give data
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 20 == 0:
         # to see the step improvement
-        print(sess.run(cross_entropy, feed_dict={xs: x_data, ys: y_data}))
+        # print(sess.run(cross_entropy, feed_dict={xs: x_data, ys: y_data}))
+        entry       = {}
+        entry[str(i)]    = str(sess.run(cross_entropy, feed_dict={xs: x_data, ys: y_data}))
+        tensorflow_to_mongodb(collection, entry)
+
+# Finish Indicator
+print("Training Results of BP Algorithm with Iris Datasets have been inserted into the database!")
 #=======================================================================================
 # Test model
 
-field    = (0,1,2,3)
-label    = (4,)
-database = "test"
-col      = "col"
-# read the first 4 columns
-x_test   = mongodb_to_tensorflow(database, col, field)
-# read the fifth column
-target_test   = mongodb_to_tensorflow(database, col, label)
+# field    = (0,1,2,3)
+# label    = (4,)
+# database = "test"
+# col      = "col"
+# # read the first 4 columns
+# x_test   = mongodb_to_tensorflow(database, col, field)
+# # read the fifth column
+# target_test   = mongodb_to_tensorflow(database, col, label)
 
 
-y_test = np.zeros((len(target_test),3))
+# y_test = np.zeros((len(target_test),3))
 
 
-for i in list(range(0, len(y_test), 1)):
-    map = {
-        0: [1, 0, 0],
-        1: [0, 1, 0],
-        2: [0, 0, 1],
-    }
-    y_test[i] = map[target_test[i][0]]
+# for i in list(range(0, len(y_test), 1)):
+#     map = {
+#         0: [1, 0, 0],
+#         1: [0, 1, 0],
+#         2: [0, 0, 1],
+#     }
+#     y_test[i] = map[target_test[i][0]]
 
 
-correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(ys,1))
-# Calculate accuracy
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#print("Result:", sess.run(tf.argmax(prediction,1), feed_dict={xs: x_test}))
-print("Accuracy:", sess.run(accuracy, feed_dict={xs: x_test, ys: y_test}))
+# correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(ys,1))
+# # Calculate accuracy
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# #print("Result:", sess.run(tf.argmax(prediction,1), feed_dict={xs: x_test}))
+# print("Accuracy:", sess.run(accuracy, feed_dict={xs: x_test, ys: y_test}))
